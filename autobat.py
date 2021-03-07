15b78397e49395bd7f1d07457bb206d0b01430e2
mind2hex <neodeus8@gmail.com>
2021-03-07 18:04:30 -0500

    autobat.py changed python3 to python in the calling of the interpreter.

--- autobat.py
+++ autobat.py
@@ -5,7 +5,7 @@ from os import getcwd as pwd
 
 info = "@echo off\n"
 info += "cd " + pwd() + "\n"
-info += comando("where python3").decode().rstrip("\r\n")
+info += comando("where python").decode().rstrip("\r\n")
 info += " main.py\n"
 info += "pause"
 
